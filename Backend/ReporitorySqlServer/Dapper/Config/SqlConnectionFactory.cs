316d5802572dc2b3cea2063ab708e0520934b1e0
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-07-31 21:00:47 -0500

    Backend: Se implementa la lectura de órdenes utilizando Dapper y se configura la conexión a SQL Server.

---
+++ Backend/ReporitorySqlServer/Dapper/Config/SqlConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace ReporitorySqlServer.Dapper.Config
+{
+    public class SqlConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
---
+++ Backend/ReporitorySqlServer/Dapper/Repositories/OrdersDapperRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using Dapper;
+using Domain.Model.Orders;
+using ReporitorySqlServer.Dapper.Config;
+
+namespace ReporitorySqlServer.Dapper.Repositories
+{
+    public class OrdersDapperRepository
+    {
+        private readonly SqlConnectionFactory _connectionFactory;
+
+        public OrdersDapperRepository(SqlConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task<IEnumerable<OrdersReadModel>> GetAsync(int id)
+        {
+            using IDbConnection db = _connectionFactory.CreateConnection();
+
+            const string query = @"
+            SELECT
+                orderid,
+                requireddate,
+                shippeddate,
+                shipname,
+                shipaddress,
+                shipcity
+            FROM [Sales].[Orders]
+            WHERE custid = @Id";
+
+            return await db.QueryAsync<OrdersReadModel>(query, new { Id = id });
+        }
+    }
+}
