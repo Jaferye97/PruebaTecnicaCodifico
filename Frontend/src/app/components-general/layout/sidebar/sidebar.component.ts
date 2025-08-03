6df6433a6a1858052e153cc10bf881496d4b1c13
Yeimer Jaramillo <andresjara0897@hotmail.com>
2025-08-02 21:13:43 -0500

    Frontend: Se agrega el componente Sidebar con funcionalidad de navegación.

---
+++ Frontend/src/app/components-general/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [MatListModule, MatIconModule, RouterModule, CommonModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css'],
+})
+export class SidebarComponent {
+  activeSection: string | null = null;
+
+  menu = [
+    {
+      title: 'Formulario',
+      key: 'formulario',
+      icon: 'storage',
+      items: [
+        { title: 'Login', key: 'login', route: 'formulario/', icon: 'storage' },
+      ],
+    },
+  ];
+
+  toggleSection(key: string) {
+    this.activeSection = this.activeSection === key ? null : key;
+  }
+}
